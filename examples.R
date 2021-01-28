a550586a245d681b6401422dfe7c44ea5932b0b3
thomasyu888 <thomas.yu@sagebase.org>
2021-01-28 20:26:32 +0800

    Add example

---
+++ examples.R
@@ -0,0 +1,24 @@
+# install from source
+devtools::install(".")
+
+library(synclient)
+
+# Interact with Challenge services
+challenge_service <- ChallengeServicesApi$new()
+# Interact with User services
+user_service <- UserProfileServicesApi$new()
+# Configure HTTP basic authorization: bearerAuth
+# Obtain Synapse PAT by going to Settings and getting PAT
+pat = ''
+challenge_service$apiClient$accessToken <- pat
+user_service$apiClient$accessToken <- pat
+# Get own user profile
+my_user = user_service$GetMyOwnUserProfile
+
+# Interact with challeng service
+challenge = challenge_service$GetChallenge(4328)
+teams = challenge_service$ListChallengeTeams(4328)
+participants = challenge_service$ListParticipantsInChallenge(4328)
+challenges = challenge_service$ListChallengesForParticipant(my_user$ownerId)
+
+
